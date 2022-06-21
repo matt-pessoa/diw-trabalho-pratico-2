df97c7e73c5397a023f8adaf7e25c64e3ed78340
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2022-06-21 00:41:59 -0300

    consome filmes populares

--- fetchData.js
+++ fetchData.js
@@ -1,17 +1,47 @@
-const baseURL = 'https://api.themoviedb.org/3/movie';
+const baseURL = 'https://api.themoviedb.org/3';
+const baseImageURL = 'https://image.tmdb.org/t/p/w500';
 const APIKEY = '4bb823474cfbfb5613a29886ce0fc79c';
 
 function getPopularMovies() {
   const popularMovies = fetch(
-    `${baseURL}/popular?api_key=${APIKEY}&language=en-US&page=1`
-  ).then((data) => data.json());
+    `${baseURL}/movie/popular?api_key=${APIKEY}&language=pt-BR`
+  )
+    .then((data) => data.json())
+    .then(({ results }) => results);
 
   return popularMovies;
 }
 
 async function generateMovieCards() {
   const popularMovies = await getPopularMovies();
-  console.log(popularMovies);
+  const cards = document.getElementById('cards');
+
+  popularMovies.forEach((movie) => {
+    const movieCard = document.createElement('div');
+    movieCard.classList.add('movie-card');
+    cards.appendChild(movieCard);
+
+    const movieImage = document.createElement('img');
+    movieImage.setAttribute('src', `${baseImageURL}/${movie.poster_path}`);
+    movieImage.classList.add('movie-card-poster');
+
+    const movieTitle = document.createElement('h2');
+    movieTitle.innerText = movie.title;
+    movieTitle.classList.add('movie-card-title');
+
+    const movieYear = document.createElement('h3');
+    movieYear.innerText = movie.release_date.slice(0, 4);
+    movieYear.classList.add('movie-card-year');
+
+    const movieRating = document.createElement('h3');
+    movieRating.innerText = movie.vote_average;
+    movieRating.classList.add('movie-card-rating');
+
+    movieCard.appendChild(movieImage);
+    movieCard.appendChild(movieTitle);
+    movieCard.appendChild(movieYear);
+    movieCard.appendChild(movieRating);
+  });
 }
 
 window.onload = () => {
