d3a1ec81c115558b9d31b1754d2fd239ba1c867a
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-06-21 17:04:40 -0300

    feat: pesquisa em tempo real

--- popularMovies.js
+++ popularMovies.js
@@ -1,57 +1,61 @@
-const baseURL = 'https://api.themoviedb.org/3';
-const baseImageURL = 'https://image.tmdb.org/t/p/w500';
-const APIKEY = '4bb823474cfbfb5613a29886ce0fc79c';
+const baseURL = "https://api.themoviedb.org/3";
+const baseImageURL = "https://image.tmdb.org/t/p/w500";
+const APIKEY = "4bb823474cfbfb5613a29886ce0fc79c";
 
 function getPopularMovies() {
-  const popularMovies = fetch(
-    `${baseURL}/movie/popular?api_key=${APIKEY}&language=pt-BR`
-  )
-    .then((data) => data.json())
-    .then(({ results }) => results);
+	const popularMovies = fetch(
+		`${baseURL}/movie/popular?api_key=${APIKEY}&language=pt-BR`
+	)
+		.then((data) => data.json())
+		.then(({ results }) => results);
 
-  return popularMovies;
+	return popularMovies;
 }
 
 async function generateMovieCards() {
-  const popularMovies = await getPopularMovies();
-  const cards = document.getElementById('cards');
+	const popularMovies = await getPopularMovies();
+	const cards = document.getElementById("cards");
 
-  popularMovies.forEach((movie) => {
-    const linkTo = document.createElement('a');
-    linkTo.setAttribute('href', 'detalhes.html');
-    linkTo.onclick = () => {
-      localStorage.setItem('id', movie.id);
-    };
-    cards.appendChild(linkTo);
+	popularMovies.forEach((movie) => {
+		const linkTo = document.createElement("a");
+		linkTo.setAttribute("href", "detalhes.html");
+		linkTo.onclick = () => {
+			localStorage.setItem("id", movie.id);
+		};
+		cards.appendChild(linkTo);
 
-    const movieCard = document.createElement('div');
-    movieCard.classList.add('movie-card');
-    linkTo.appendChild(movieCard);
+		const movieCard = document.createElement("div");
+		movieCard.classList.add("movie-card");
+		linkTo.appendChild(movieCard);
 
-    const movieImage = document.createElement('img');
-    movieImage.setAttribute('src', `${baseImageURL}/${movie.poster_path}`);
-    movieImage.setAttribute('alt', movie.title);
-    movieImage.classList.add('movie-card-poster');
+		const movieImage = document.createElement("img");
+		movieImage.setAttribute("src", `${baseImageURL}/${movie.poster_path}`);
+		movieImage.setAttribute("alt", movie.title);
+		movieImage.classList.add("movie-card-poster");
 
-    const movieTitle = document.createElement('h2');
-    movieTitle.innerText = movie.title;
-    movieTitle.classList.add('movie-card-title');
+		const movieTitle = document.createElement("h2");
+		movieTitle.innerText = movie.title;
+		movieTitle.classList.add("movie-card-title");
 
-    const movieYear = document.createElement('h3');
-    movieYear.innerText = movie.release_date.slice(0, 4);
-    movieYear.classList.add('movie-card-year');
+		const movieYear = document.createElement("h3");
+		movieYear.innerText = movie.release_date.slice(0, 4);
+		movieYear.classList.add("movie-card-year");
 
-    const movieRating = document.createElement('h3');
-    movieRating.innerText = movie.vote_average;
-    movieRating.classList.add('movie-card-rating');
+		const movieRating = document.createElement("h3");
+		movieRating.innerText = movie.vote_average;
+		movieRating.classList.add("movie-card-rating");
 
-    movieCard.appendChild(movieImage);
-    movieCard.appendChild(movieTitle);
-    movieCard.appendChild(movieYear);
-    movieCard.appendChild(movieRating);
-  });
+		movieCard.appendChild(movieImage);
+		movieCard.appendChild(movieTitle);
+		movieCard.appendChild(movieYear);
+		movieCard.appendChild(movieRating);
+	});
 }
 
 window.onload = () => {
-  generateMovieCards();
+	updateSearch();
+	const searchBar = document.getElementById("search-bar");
+	searchBar.addEventListener("input", (event) => {
+		updateSearch(event.target.value);
+	});
 };
--- search.js
+++ search.js
@@ -1,9 +1,67 @@
-function fetchQuery(query) {
-  const response = fetch(
-    `${baseURL}/search/company/?api_key=${APIKEY}&query=${query}page=1`
-  )
-    .then((data) => data.json())
-    .then((result) => console.log(result));
+function getQueryMovies(query) {
+	const response = fetch(
+		`${baseURL}/search/movie?api_key=${APIKEY}&language=pt-BR&query=${query}`
+	)
+		.then((data) => data.json())
+		.then((result) => result);
 
-  return response;
+	return response;
+}
+
+async function generateQueryCards(query) {
+	const { results } = await getQueryMovies(query);
+	console.log(results);
+	const cards = document.getElementById("cards");
+
+	results.forEach((movie) => {
+		const linkTo = document.createElement("a");
+		linkTo.setAttribute("href", "detalhes.html");
+		linkTo.onclick = () => {
+			localStorage.setItem("id", movie.id);
+		};
+		cards.appendChild(linkTo);
+
+		const movieCard = document.createElement("div");
+		movieCard.classList.add("movie-card");
+		linkTo.appendChild(movieCard);
+
+		const movieImage = document.createElement("img");
+		movieImage.setAttribute("src", `${baseImageURL}/${movie.poster_path}`);
+		movieImage.setAttribute("alt", movie.title);
+		movieImage.classList.add("movie-card-poster");
+
+		const movieTitle = document.createElement("h2");
+		movieTitle.innerText = movie.title;
+		movieTitle.classList.add("movie-card-title");
+
+		const movieYear = document.createElement("h3");
+		movieYear.innerText = movie.release_date;
+		movieYear.classList.add("movie-card-year");
+
+		const movieRating = document.createElement("h3");
+		movieRating.innerText = movie.vote_average;
+		movieRating.classList.add("movie-card-rating");
+
+		movieCard.appendChild(movieImage);
+		movieCard.appendChild(movieTitle);
+		movieCard.appendChild(movieYear);
+		movieCard.appendChild(movieRating);
+	});
+}
+
+function removeChildren() {
+	const cards = document.getElementById("cards");
+	cards.innerHTML = "";
+}
+
+function updateSearch(query) {
+	const searchBar = document.getElementById("search-bar");
+
+	if (searchBar.value === "") {
+		removeChildren();
+		generateMovieCards();
+	} else {
+		removeChildren();
+		generateQueryCards(query);
+	}
 }
